3ffd7ffdb088af9e04b3350861d7a2c44d1a6b9a
c-rter <c@rterlawson.com>
2021-05-18 17:09:45 -0400

    integrated swagger route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 require('dotenv').config();
 
@@ -15,11 +17,33 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "TEST 1",
+			version: "1.0.0",
+			description: "TEST 3",
+		},
+		servers: [
+			{
+				url: "https://rest-api-docs.herokuapp.com/",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋 SURVEY HERE 🌎🌍🌏✨🌈🦄'
   });
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use('/api/v1', api);
 
